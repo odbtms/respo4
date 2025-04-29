cbf6860c8cf95f2c0c4c9d0031b7f4e89fcd355e
tomas <to.cortess@duocuc.cl>
2025-04-29 13:41:14 -0400

    first commit

---
+++ src/main/java/com/seccion4/respo4/Resspo4Application.java
@@ -0,0 +1,13 @@
+package com.seccion4.respo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resspo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resspo4Application.class, args);
+	}
+
+}
